a87ed0c37f228ec4ecac56018a0d77ed24325e34
Andrei Marchuk <andm86@gmail.com>
2019-08-04 14:11:41 +0300

    Task_14 add test to compare contact properties from home and edit page

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,6 +1,6 @@
 import os
 from model.contact import Contact
-import time
+import re
 
 
 class ContactHelper:
@@ -23,14 +23,27 @@ class ContactHelper:
     def edit_some_contact(self, contactEdit, index):
         wd = self.app.wd
         # start edit contact
-        xpath_index = str(index+1)
-        wd.find_element_by_xpath("//tr[@name = 'entry']["+xpath_index+"]//td[8]").click()
+        self.open_contact_to_edit_by_index(index)
         # fill contact form
         self.fill_contact(contactEdit, wd)
         # update contact
         wd.find_element_by_css_selector("[name = update]:last-child").click()
         self.app.return_to_homepage()
         self.contact_cache = None
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
+        # xpath_index = str(index + 1)
+        # wd.find_element_by_xpath("//tr[@name = 'entry'][" + xpath_index + "]//td[8]").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
 
     def fill_contact(self, contact, wd):
         self.app.change_value_by_name("firstname", contact.firstname)
@@ -93,9 +106,41 @@ class ContactHelper:
                 id = element.find_element_by_name("selected[]").get_attribute("value")
                 lastname = element.find_element_by_xpath("td[2]").text
                 firstname = element.find_element_by_xpath("td[3]").text
-                self.contact_cache.append(Contact(id=id, lastname=lastname, firstname=firstname))
+                address = element.find_element_by_xpath("td[4]").text
+                all_emails = element.find_element_by_xpath("td[5]").text
+                all_phones = element.find_element_by_xpath("td[6]").text
+                self.contact_cache.append(Contact(id=id, lastname=lastname, firstname=firstname, address=address,
+                                  all_phones_from_home_page=all_phones, all_emails_from_home_page=all_emails))
         return list(self.contact_cache)
 
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        address = wd.find_element_by_name("address").get_attribute("value")
+        home = wd.find_element_by_name("home").get_attribute("value")
+        mobile = wd.find_element_by_name("mobile").get_attribute("value")
+        work = wd.find_element_by_name("work").get_attribute("value")
+        phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
+
+        return Contact(firstname=firstname, lastname=lastname, id=id, address=address, home=home, mobile=mobile,
+                       work=work, phone2=phone2, email=email, email2=email2, email3=email3)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home = re.search("H: (.*)", text).group(1)
+        work = re.search("W: (.*)", text).group(1)
+        mobile = re.search("M: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Contact(home=home, mobile=mobile, work=work, phone2=phone2)
 
 
     # def edit(self, searched_lastname, searched_firstname, contactEdit):
--- test/test_contact.py
+++ test/test_contact.py
@@ -2,6 +2,7 @@
 import time
 from model.contact import Contact
 from random import randrange
+import re
 
 
 def test_add_contact(app):
@@ -9,11 +10,11 @@ def test_add_contact(app):
     contact = Contact(firstname="test firstname", middlename="test middlename",
                                lastname="test lastname", nickname="test nickname", photo="C:\\fakepath\\Untitled.jpg",
                                title="test title", company="test company", address="test address",
-                               home="test home", mobile="test mobile", work="test work", fax="test fax",
+                               home="testhome12345", mobile="testmobile12345", work="testwork12345", fax="testfax12345",
                                email="test email", email2="test email2", email3="test email3",
                                homepage="test homepage", birthday="11 May 1977",
                                anniversary="3 June 1959", address2="test address2",
-                               phone2="test phone2", notes="test notes")
+                               phone2="testphone2", notes="test notes")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
@@ -28,11 +29,11 @@ def test_edit_contact(app):
     contact = Contact(firstname="editTest firstname", middlename="editTest middlename",
                                lastname="editTest lastname", nickname="editTest nickname", photo="C:\\fakepath\\Untitled.jpg",
                                title="editTest title", company="editTest company", address="editTest address",
-                               home="editTest home", mobile="editTest mobile", work="editTest work", fax="editTest fax",
-                               email="editTest email", email2="editTest email2", email3="editTest email3",
-                               homepage="editTest homepage", birthday="25 May 1980",
+                               home="editTesthome54321", mobile="editTestmobile54321", work="editTestwork54321",
+                               fax="editTestfax54321", email="editTest email", email2="editTest email2",
+                               email3="editTest email3", homepage="editTest homepage", birthday="25 May 1980",
                                anniversary="25 June 1980", address2="editTest address2",
-                               phone2="editTest phone2", notes="editTest notes")
+                               phone2="editTestphone2", notes="editTest notes")
     contact.id = old_contacts[index].id
     app.contact.edit_some_contact(contact, index)
     assert len(old_contacts) == app.contact.count()
@@ -51,3 +52,27 @@ def test_delete_contact(app):
     new_contacts = app.contact.get_contact_list()
     old_contacts[index:index+1] = []
     assert old_contacts == new_contacts
+
+def test_compare_some_contact_data_from_home_edit_pages(app):
+    index = randrange(app.contact.count())
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.address == contact_from_edit_page.address
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                                map(lambda x: clear(x),
+                                    filter(lambda x: x is not None,
+                                           [contact.home, contact.mobile, contact.work, contact.phone2]))))
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3])))
