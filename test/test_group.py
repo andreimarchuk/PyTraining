b1c828e9a8652c1c55ba89d675c9dc8d91d09515
Andrei Marchuk <andm86@gmail.com>
2019-08-22 08:38:20 +0300

    Save commit

--- test/test_group.py
+++ test/test_group.py
@@ -7,12 +7,12 @@ from data.groups import testdata
 
 #@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
+    #assert len(old_groups) + 1 == app.group.count()
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
@@ -30,16 +30,21 @@ def test_edit_some_group(app):
     old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_delete_some_group(app):
-    if app.group.count() == 0:
+
+def test_delete_some_group(app, db, check_ui):
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(name="testEdit group"))
-    old_groups = app.group.get_group_list()
-    index = random.randrange(len(old_groups))
-    app.group.delete_group_by_index(index)
-    assert len(old_groups) - 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index:index+1] = []
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    app.group.delete_group_by_id(group.id)
+    new_groups = db.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
+
 
 def test_delete_all_groups(app):
     app.group.delete_all_groups()
