c1a44b83d265e904bde5a4eb135070b6525e05c1
Andrei Marchuk <andm86@gmail.com>
2019-09-08 17:03:39 +0300

    Update bdd tests for allure report

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # PyTraining
 
 Repository
+
+
+2. Install and activate virtual environment
+3. Run -->  python -m pip install allure-pytest
+4. Run -->  python -m pip install -r requirements.txt
+5. Run -->  python -m pytest bdd/ --alluredir=bdd/reports -vvv --gherkin-terminal-reporter
+6. Run -->  allure serve bdd/reports
--- bdd/test_contact.py
+++ bdd/test_contact.py
@@ -1,5 +1,6 @@
 from pytest_bdd import given, when, then, scenario
 from model.contact import Contact
+import allure
 import random
 
 
@@ -26,11 +27,13 @@ def new_contact(firstname, lastname):
 
 
 @when('I add the contact to the list')
+@allure.step('I add the contact to the list')
 def add_new_contact(app, new_contact):
     app.contact.create(new_contact)
 
 
 @then('the new contact list is equal to the old list with the added contact')
+@allure.step('the new contact list is equal to the old list with the added contact')
 def verify_contact_added(db, contact_list, new_contact):
     old_contacts = contact_list
     new_contacts = db.get_contact_list()
@@ -48,12 +51,14 @@ def random_contact(non_empty_contact_list):
     return random.choice(non_empty_contact_list)
 
 @when('I delete the contact from the list')
+@allure.step('I delete the contact from the list')
 def delete_contact(app, random_contact):
     index_on_page = app.contact.index_of_contact_on_page(random_contact)
     app.contact.delete_some_contact(index_on_page)
 
 
 @then('the new contact list is equal to the old list without deleted contact')
+@allure.step('the new contact list is equal to the old list without deleted contact')
 def verify_contact_deleted(db, non_empty_contact_list, random_contact):
     old_contacts = non_empty_contact_list
     new_contacts = db.get_contact_list()
@@ -62,12 +67,14 @@ def verify_contact_deleted(db, non_empty_contact_list, random_contact):
     assert old_contacts == new_contacts
 
 @when('I edit the contact from the list')
+@allure.step('I edit the contact from the list')
 def edit_contact(app, random_contact, new_contact):
     new_contact.id = random_contact.id
     index_on_page = app.contact.index_of_contact_on_page(random_contact)
     app.contact.edit_some_contact(new_contact, index_on_page)
 
 @then('the new contact list is equal to the old list with edited contact')
+@allure.step('the new contact list is equal to the old list with edited contact')
 def verify_contact_edited(db, non_empty_contact_list, random_contact, new_contact, app):
     old_contacts = non_empty_contact_list
     new_contacts = db.get_contact_list()
